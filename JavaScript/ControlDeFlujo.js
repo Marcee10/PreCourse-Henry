e583b29ae141ead26b5ec9179e880cd4c806c72f
Marcee10 <marceloelopez.ml@gmail.com>
2024-03-07 14:20:26 -0300

    avance marzo

---
+++ JavaScript/ControlDeFlujo.js
@@ -0,0 +1,26 @@
+function viajar(destino) {
+    if(destino === 'Brasil') {
+             console.log('Gire a la izquierda');
+    } else if(destino === 'Argentina') {
+             console.log('Gire a la derecha');
+    } else {
+             console.log('Nos perdimos');''
+    }
+  };
+  viajar('Brasil');
+
+
+
+
+  function puedeManejar(edad) {
+    if(edad >= 18) {
+             console.log( true );
+    } 
+    else
+    console.log( false );
+  }
+  
+  puedeManejar(18);
+
+ 
+ 
---
+++ JavaScript/operadores.js
@@ -0,0 +1,4 @@
+console.log ("HOLA "+"CAMILO") ;
+var nombre = "CAMILO, SOY UNA MAQUINA";
+var saludo = "HOLA " + nombre;
+console.log (saludo);
